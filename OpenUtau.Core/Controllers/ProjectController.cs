066cef6e094825dda378b930b7640e780922d6a1
LingXuanYin <3546599908@qq.com>
2025-05-03 22:13:48 +0800

    init http server

---
+++ OpenUtau.Core/Controllers/ProjectController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace OpenUtau.Core.Controllers {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjectController : ControllerBase {
+        [HttpGet]
+        public IActionResult Get() {
+            Log.Information("Received GET request for project");
+            return Ok(new {
+                status = "ok",
+                message = "OpenUtau HTTP API is running"
+            });
+        }
+    }
+} 
